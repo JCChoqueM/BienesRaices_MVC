8fbad2104022950eb490e040e76b230cc98ae642
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-08-06 01:16:52 -0400

    Add basic routing functionality to Router class
    Introduces GET route registration and route checking in the Router class. Updates index.php to register several GET routes and invoke route checking. This lays the foundation for handling different URLs in the application.

--- Router.php
+++ Router.php
@@ -1,7 +1,32 @@
 <?php
 namespace MVC;
 class Router{
-    public function __construct(){
-        echo "Creando el Router...";
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+    public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas(){
+       $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+  
+       $metodo = $_SERVER['REQUEST_METHOD'];
+
+       if ($metodo === 'GET') {
+
+        $fn = $this->rutasGET[$urlActual]?? null;
+           
+       } else {
+           $fn = $this->rutasPOST[$urlActual] ?? null;
+       }
+
+       if ( $fn ) {
+        //La url existe y hay una fn asociada
+          call_user_func($fn, $this);
+       }else{
+           echo "Pagina no encontrada o no disponible";
+       }
+
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 require_once __DIR__ . '/../includes/app.php';
+
 use MVC\Router;
+
 $router = new Router();
-debuguear($router);
+
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/anuncios', 'funcion_anuncios');
+$router->get('/contacto', 'funcion_contacto');
+$router->get('/blog', 'funcion_blog');
+$router->get('/propiedad', 'funcion_propiedad');
+
+
+
+$router->comprobarRutas();
