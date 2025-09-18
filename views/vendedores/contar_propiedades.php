88cb9568c205f0a2748443a5f233a433d347d9f7
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-09-18 19:52:23 -0400

    eliminado archiovo vededores contar_Propiedades

--- views/vendedores/contar_propiedades.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../../includes/config/database.php';
-$db = conectarDB();
-$query = "SELECT COUNT(*) as total FROM propiedades";
-$resultado = mysqli_query($db, $query);
-$row = mysqli_fetch_assoc($resultado);
-echo $row['total'];
