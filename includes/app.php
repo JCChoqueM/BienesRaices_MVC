b4d7738123f29ac7f721569e1340487a05849cd1
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-12-16 15:57:15 -0400

    primeros 3 pasos

--- includes/app.php
+++ includes/app.php
@@ -1,11 +1,13 @@
 <?php
+use Model\ActiveRecord;
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 require 'funciones.php';
 require 'database.php';
-require __DIR__ . '/../vendor/autoload.php';
 
 //Conectarnos a la base de Datos
 $db = conectarDB();
 
-use Model\ActiveRecord;
 
 ActiveRecord::setDB($db);
